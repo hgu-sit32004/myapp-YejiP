96bbf2e32624c4847daf044a162b3a908fb51f11
student38 <student38@student38.local>
2018-05-25 15:26:38 +0900

    init

---
+++ Desktop/new/21500280/visionboard_2/visionboard/Main/add/addcontents.swift
@@ -0,0 +1,28 @@
+
+import UIKit
+
+class addcontents: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBOutlet weak var img: UIImageView!
+    
+    
+    let imagepicker = UIImagePickerController()
+    
+    @IBAction func btn_pick(_ sender: Any) {
+        imagepicker.delegate = self
+        imagepicker.sourceType = UIImagePickerControllerSourceType.savedPhotosAlbum
+        self.present(imagepicker , animated : true , completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            img.image = image
+        }
+    
+    dismiss(animated: true, completion: nil)
+    }
+}
